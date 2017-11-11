b7e281f6a5854439f40a41a6e21ba8dbdb04704d
Samuel0c <sleppiniemi@protonmail.com>
2017-11-11 20:02:04 +0200

    singletonit pois

--- Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Pankki.java
+++ Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Pankki.java
@@ -1,20 +1,11 @@
 package ohtu.verkkokauppa;
 
 public class Pankki implements PankkiInterface {
+    
+    private Kirjanpitonterface kirjanpito;
 
-    private static Pankki instanssi;
-
-    public static Pankki getInstance() {
-        if (instanssi == null) {
-            instanssi = new Pankki();
-        }
-
-        return instanssi;
-    }
-    private Kirjanpito kirjanpito;
-
-    public Pankki() {
-        kirjanpito = Kirjanpito.getInstance();
+    public Pankki(Kirjanpitonterface kirjanpito) {
+        this.kirjanpito = kirjanpito;
     }
 
     @Override
